3bb5538c0865d56b92d96964548ea2dd7611734b
unknown <vlaovicaksel@gmail.com>
2026-07-14 06:09:32 -0500

    fe: Display XP progress on run overview

--- frontend/battleforge-frontend/src/app/run-overview/run-overview.component.ts
+++ frontend/battleforge-frontend/src/app/run-overview/run-overview.component.ts
@@ -55,4 +55,19 @@ export class RunOverviewComponent implements OnInit {
 
   }
 
+  xpThreshold(): string {
+
+    switch(this.run?.hero.level) {
+
+      case 1: return "100";
+      case 2: return "200";
+      case 3: return "350";
+      case 4: return "500";
+      case 5: return "MAX";
+      default:  return "";
+      
+    }
+
+  }
+
 }
